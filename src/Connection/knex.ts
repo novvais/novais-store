f6d1f95d54212e1c5511757f54f88cdef6133400
Eduardo Novais <eduardodepaulanovais10@gmail.com>
2024-03-15 19:52:12 -0300

    adding and configuring knex

--- src/Connection/knex.ts
+++ src/Connection/knex.ts
@@ -1,13 +1,12 @@
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    },
-  });
-  
-  module.exports = knex;
-  
+import conection from "knex";
+
+export const knex = conection({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+});
