781f39683aeafafc3fae190d2f3adc3bb4dfad19
Eduardo Novais <eduardodepaulanovais10@gmail.com>
2024-05-30 15:46:56 -0300

    creating controller and services to sell

---
+++ src/Controllers/sellController.ts
@@ -0,0 +1,5 @@
+export class SellController {
+    async registerSellController(req: Request, res: Response) {
+
+    }
+}
---
+++ src/Services/sellServices.ts
@@ -0,0 +1,8 @@
+import { knex } from "../Connection/knex";
+
+export class SellServices {
+  static async registerSell() {}
+
+  static async calculateFreight() {
+  }
+}
