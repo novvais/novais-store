d562558c71eaf2f15a35d7f542737154083d6300
Eduardo Novais <eduardodepaulanovais10@gmail.com>
2024-05-30 15:44:54 -0300

    configure nodemailer and aws s3

--- src/Connection/aws.ts
+++ src/Connection/aws.ts
@@ -1,14 +1,14 @@
-import aws from "aws-sdk";
-require("aws-sdk/lib/maintenance_mode_message").suppress = true;
+// import aws from "aws-sdk";
+// require("aws-sdk/lib/maintenance_mode_message").suppress = true;
 
-const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
+// const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
 
-const s3 = new aws.S3({
-  endpoint,
-  credentials: {
-    accessKeyId: process.env.KEY_ID,
-    secretAccessKey: process.env.APP_KEY,
-  },
-});
+// const s3 = new aws.S3({
+//   endpoint,
+//   credentials: {
+//     accessKeyId: process.env.KEY_ID,
+//     secretAccessKey: process.env.APP_KEY,
+//   },
+// });
 
-export { s3 }
+// export { s3 }
--- src/Connection/nodemailer.js
+++ src/Connection/nodemailer.js
@@ -1,21 +1,21 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
+// const transport = nodemailer.createTransport({
+//   host: process.env.MAIL_HOST,
+//   port: process.env.MAIL_PORT,
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
 
-const send = (to, subject, body) => {
-  transport.sendMail({
-    from: process.env.MAIL_FROM,
-    to,
-    subject,
-    text: body,
-  });
-};
+// const send = (to, subject, body) => {
+//   transport.sendMail({
+//     from: process.env.MAIL_FROM,
+//     to,
+//     subject,
+//     text: body,
+//   });
+// };
 
-module.exports = send;
+// module.exports = send;
